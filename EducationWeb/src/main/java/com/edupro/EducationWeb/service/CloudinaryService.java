93b73cb4a9c087915be1d4f1e0539776695645a6
TalukderSiam <124248232+TalukderSiam@users.noreply.github.com>
2025-05-11 15:15:33 +0600

    Delete EducationWeb directory

--- EducationWeb/src/main/java/com/edupro/EducationWeb/service/CloudinaryService.java
+++
@@ -1,9 +0,0 @@
-package com.edupro.EducationWeb.service;
-
-import org.springframework.web.multipart.MultipartFile;
-
-public interface CloudinaryService {
-
-    String uploadFile(MultipartFile file);
-    
-}
